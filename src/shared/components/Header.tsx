feae5697fde737bf2792c984d197cbe89788fbe2
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2026-04-28 13:11:30 -0300

    responsive header for mobile

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -3,13 +3,12 @@ import { SearchBar } from "./SearchBar";
 
 export const Header = () => {
   return (
-    <header className="flex justify-center items-center bg-[#0000FF] py-4">
-      <div className="flex items-center gap-6 w-full max-w-4xl px-4">
-        <div>
+    <header className="bg-[#0000FF] py-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-3 md:gap-6w-full max-w-4xl mx-auto px-4">
+        <div className="flex justify-center md:justify-start">
           <ImageComponent />
         </div>
-
-        <div className="flex flex-1 items-center bg-white rounded px-3 py-1.5">
+        <div className="w-full md:flex-1">
           <SearchBar />
         </div>
       </div>
